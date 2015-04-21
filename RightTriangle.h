22c2d6a49142067c0bf5425afe15e669b03693c2
Christopher Moore <mrmepper@gmail.com>
2015-04-21 09:57:06 -0500

    Create RightTriangle.h
    Derived RightTriangle from base Triangle

---
+++ RightTriangle.h
@@ -0,0 +1,16 @@
+#ifndef RIGHT_TRIANGLE_H
+#define RIGHT_TRIANGLE_H
+
+#include "Triangle.h"
+
+class RightTriangle : public Triangle
+{
+ 
+  public:
+
+    RightTriangle();
+    RightTriangle(GenPoint vertex, int height, int base, Color color);
+    void print();
+};
+
+#endif
