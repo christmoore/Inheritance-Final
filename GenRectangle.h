f41ba5bdb213baca4d3dec9a69e9b9502b799bba
Christopher Moore <mrmepper@gmail.com>
2015-04-21 10:21:15 -0500

    Create GenRectangle.h

---
+++ GenRectangle.h
@@ -0,0 +1,17 @@
+#ifndef GENRECTANGLE_H
+#define GENRECTANGLE_H
+
+#include "Quadrilateral.h"
+
+class GenRectangle: public Quadrilateral
+{
+  
+  public:
+    GenRectangle();
+    GenRectangle(GenPoint ul, GenPoint lr, Color c);
+    void setPoints(GenPoint ul, GenPoint lr);
+    double getArea();
+    void print();
+};
+
+#endif
