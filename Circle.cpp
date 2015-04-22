2acae635dfca1ff4e31164979c89011eaa6f923d
judDickey1 <judson.dickey@gmail.com>
2015-04-21 21:31:44 -0500

    Update Circle.cpp

--- Circle.cpp
+++ Circle.cpp
@@ -1,11 +1,30 @@
+#include <iostream>
 #include "Circle.h"
+#include "GenPoint.h"
+#include "Color.h"
 
 
-Circle::Circle(void)
+Circle::Circle()
 {
 }
-
-
-Circle::~Circle(void)
+Circle::Circle(GenPoint center, int radius, Color c)
+{
+}
+void Circle::setCenter(GenPoint center)
+{
+}
+void Circle::setRadius(int radius)
+{
+}
+double Circle::getPerimeter()
+{
+}
+double Circle::getArea()
+{
+}
+void Circle::print()
+{
+}
+void Circle::draw()
 {
 }
